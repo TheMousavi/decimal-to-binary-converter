6f7e7aa7f0ef00df1ab8cb23cdfc94d7676eaa71
TheMousavi <mousavi.amir1616@gmail.com>
2025-05-06 05:58:39 +0330

    Optimization decimalToBinary && checkUserInput

--- script.js
+++ script.js
@@ -1,40 +1,33 @@
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
+const animationContainer = document.getElementById("animation-container");
 
 const decimalToBinary = (input) => {
-    const inputs = [];
-    const quotients = [];
-    const remainders = [];
-
-    while(input > 0) {
-        const quotient = Math.floor(input / 2);
-        const remainder = input % 2;
-
-        inputs.push(input);
-        quotients.push(quotient);
-        remainders.push(remainder);
-        input = quotient;
-
+    if (input === 0 || input === 1) {
+      return String(input);
+    } else {
+      return decimalToBinary(Math.floor(input / 2)) + (input % 2);
     }
-    console.log("Inputs: ", inputs);
-    console.log("Quotients: ", quotients);
-    console.log("Remainders: ", remainders);
-
-    
-};
+  };
 
 const checkUserInput = () => {
-    if(    !numberInput.value ||
-        isNaN(parseInt(numberInput.value)) ||
-        parseInt(numberInput.value) < 0)
-    {
-        alert("Please enter a valid number");
+    const inputInt = parseInt(numberInput.value);
+
+    if (!numberInput.value || isNaN(inputInt) || inputInt < 0) {
+        alert("Please provide a decimal number greater than or equal to 0");
         return;
-    }
-    decimalToBinary(parseInt(numberInput.value));
-    numberInput.value = "";
-};
+      }
+    
+      if (inputInt === 5) {
+        showAnimation();
+        return;
+      }
+    
+      result.textContent = decimalToBinary(inputInt);
+      numberInput.value = "";
+    };
+        
 convertBtn.addEventListener("click", checkUserInput);
 
 numberInput.addEventListener("keydown" , (e) => {
